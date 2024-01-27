83550a33e215403b75e924898c3746e42f57c96d
pingu <truepinguu@gmail.com>
2024-01-27 00:26:26 +0000

    Testing if invalid urls are rejected

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,3 +3,12 @@ from peek import utils
 
 def test_EmptyStringIsRejected():
     assert utils.validateURL('') == False
+
+def test_BadUrlIsRejected():
+    assert utils.validateURL('notgood//notgood.url') == False
+
+def test_BadUrlIsRejected2():
+    assert utils.validateURL('notgood') == False
+
+def test_BadUrlIsRejected3():
+    assert utils.validateURL('.//.2.url') == False
