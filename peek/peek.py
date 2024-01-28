c7520715a7631ebbaa1bc0208d557a5be48cd27d
pingu <truepinguu@gmail.com>
2024-01-28 17:30:15 +0000

    Refactoring to remove long method code smell

--- peek/peek.py
+++ peek/peek.py
@@ -1,7 +1,7 @@
 import typer
 import os
 import requests
-from utils import validateURL, getScriptLocation
+from utils import validateURL, UrlStatus
 from rich import print
 from typing_extensions import Annotated
 
@@ -11,33 +11,31 @@ app = typer.Typer()
 def main(target_url: Annotated[str, typer.Argument()], wordlist_path: Annotated[str, typer.Argument()]):
     print("Welcome to peek v0.1")
 
-    # Checking if target url is a valid url and is reachable
-    if validateURL(target_url):
-        try:
-            response = requests.head(target_url, timeout=1)
-
-        except Exception as err:
-            print('Target does not appear to be up.')
-        
-        else:
-            if response.status_code == 200:
-                print('Target website up!')
-    else:
-        print('URL seems to be badly formed!')
+    # Checking if target url is a valid url and is reachable and if the wordlist exists
+    match validateURL(target_url):
+        case UrlStatus.UNREACHABLE:
+            print('The target %s appears to be unreachable.', target_url)
+        case UrlStatus.BADLY_FORMED:
+            print('The target %s appears to be a badly formed URL.', target_url)
+        case UrlStatus.UP:
+            print('The target %s appears to be up!', target_url)
+            if os.path.exists(wordlist_path):
+                dirRecon(target_url, wordlist_path)
+            else:
+                print('Wordlist file "%s" does not exist.', wordlist_path)
     
-    # Accessing wordlist   
+def dirRecon(target_url: str, wordlist_path: str):       
+    print('Starting direcotory recon')
     
-    if os.path.exists(wordlist_path):
-        print('Starting directory recon')
+    wordlist = open(wordlist_path, 'r')
 
-        wordlist = open(wordlist_path, 'r')
-        for word in wordlist:
-            c_url = target_url + '/' + word
-            print(c_url.strip())
-            response = requests.head(c_url.strip(), allow_redirects=True, timeout=5)
-            print(response.status_code)
-            if response.status_code != 404:
-                print('/' + word + ': EXISTS')
+    for word in wordlist:
+        c_url = target_url + '/' + word
+        print(c_url.strip())
+        response = requests.head(c_url.strip(), allow_redirects=True, timeout=5)
+        print(response.status_code)
+        if response.status_code != 404:
+            print('/' + word + ': EXISTS')
 
     else:
         print("File does not exist!")
